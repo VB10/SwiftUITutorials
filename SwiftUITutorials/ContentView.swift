985764c46dd2210bacb2b8e516fe76edf0a12b4a
Veli Bacik <vbacik.10@gmail.com>
2021-09-06 01:40:12 +0300

    hiecry spacer and aligment has done

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        sampleStackView()
+        VStack(alignment: .center){
+            Rectangle().frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Circle().foregroundColor(.red).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Text("Sampole1")
+            Text("Sampole2")
+            Text("Sampole3")
+            Spacer()
+            HStack(){
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                ZStack{
+                    Circle()
+                    Text("hello").font(.largeTitle).fontWeight(.heavy).foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0))
+                }.frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+               
+            }.frame(height: 500,alignment: Alignment.top)
+        }
+        
     }
         
     
