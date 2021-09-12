da8f042915a2b8d895d6acb77dc0f7ddbe7d6ccd
Veli Bacik <vbacik.10@gmail.com>
2021-09-13 01:58:26 +0300

    group symbol and xcode usage

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -8,30 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     var body: some View {
-        VStack(alignment: .center){
+        VStack {
+
+            Group {
+                //            MARK: Login View
+                //            TODO: Fix user name!
+                VStack {
+                    Text("Hello")
+                        .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .font(/*@START_MENU_TOKEN@*/.callout/*@END_MENU_TOKEN@*/)
+                }.frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color("color_random")
+                                                                                                                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/))
+            }
+            Group {
+                //            MARK: Register View
+                VStack {
+                    Image(systemName: "moon.fill").foregroundColor(.red)
+                    Text("Hello2")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
+    }
+
+
+    fileprivate func demoView() -> some View {
+        return VStack(alignment: .center) {
             Rectangle().frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Circle().foregroundColor(.red).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("Sampole1")
             Text("Sampole2")
             Text("Sampole3")
             Spacer()
-            HStack(){
+            HStack() {
                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                ZStack{
+                ZStack {
                     Circle()
                     Text("hello").font(.largeTitle).fontWeight(.heavy).foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0))
                 }.frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-               
-            }.frame(height: 500,alignment: Alignment.top)
+
+            }.frame(height: 500, alignment: Alignment.top)
         }
-        
     }
-        
-    
+
     fileprivate func sampleHorizontalView() -> some View {
-        return HStack{
+        return HStack {
             Spacer().frame(width: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("@vb10")
                 .padding()
@@ -45,9 +69,9 @@ struct ContentView: View {
             Spacer().frame(width: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
     }
-    
+
     fileprivate func sampleVerticalView() -> some View {
-        return VStack{
+        return VStack {
             Spacer().frame(width: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("@vb10")
                 .padding()
@@ -61,9 +85,9 @@ struct ContentView: View {
             Spacer().frame(width: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
     }
-    
+
     fileprivate func sampleStackView() -> some View {
-        return ZStack(alignment: Alignment.bottom){
+        return ZStack(alignment: Alignment.bottom) {
             Color.red
             Spacer().frame(width: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("@vb10")
