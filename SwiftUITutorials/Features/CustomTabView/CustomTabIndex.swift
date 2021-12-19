ef9c701e37bb78221d4ef3f460c87d7cf0d8ac93
Veli Bacik <vbacik.10@gmail.com>
2021-12-20 00:29:45 +0300

    Tabbar has done'

---
+++ SwiftUITutorials/Features/CustomTabView/CustomTabIndex.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTabIndex.swift
+//  SwiftUITutorials
+//
+//  Created by Veli Bacik on 20.12.2021.
+//
+
+import Foundation
+
+enum CustomTabs: Int {
+    case home = 0
+    case heart = 1
+}
+
