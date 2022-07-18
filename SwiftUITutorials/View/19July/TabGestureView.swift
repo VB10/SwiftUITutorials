385faa9ec0fcb53a8413a572a32a5b0f37b3130e
Veli Bacik <vbacik.10@gmail.com>
2022-07-19 00:44:00 +0300

    updated

---
+++ SwiftUITutorials/View/19July/TabGestureView.swift
@@ -0,0 +1,20 @@
+//
+//  TabGestureView.swift
+//  SwiftUITutorials
+//
+//  Created by Veli Bacik on 19.07.2022.
+//
+
+import SwiftUI
+
+struct TabGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabGestureView()
+    }
+}
---
+++ SwiftUITutorials/View/19July/TextEditingView.swift
@@ -0,0 +1,20 @@
+//
+//  TextEditingView.swift
+//  SwiftUITutorials
+//
+//  Created by Veli Bacik on 19.07.2022.
+//
+
+import SwiftUI
+
+struct TextEditingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextEditingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditingView()
+    }
+}
